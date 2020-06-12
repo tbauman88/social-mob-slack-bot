a9208b43be810c19b74a79aedcb4d651e9abfd0a
Tyler Bauman <t.bauman@vehikl.com>
2020-06-12 11:27:16 -0400

    [#1] feat: update/delete/added functions

---
+++ functions/mob-scheduler.js
@@ -0,0 +1,51 @@
+// @ts-check
+const fetch = require('node-fetch').default;
+
+const getMobAttendees = async (attendees) => {
+  const url = `https://slack.com/api/users.list?token=${process.env.TOKEN}`;
+  const { members } = await fetch(url).then((res) => res.json());
+  const users = members
+    .filter((m) => !m.deleted && !m.is_bot && !m.is_restricted)
+    .map(({ real_name, id }) => ({ id, name: real_name }));
+
+  let slackUsers = [];
+
+  attendees.forEach((attendee) => {
+    const person = users.find((p) => p.name === attendee);
+    person && slackUsers.push(`<@${person.id}>`);
+  });
+
+  return slackUsers.join(' ');
+};
+
+exports.handler = async function (event, context, callback) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 503,
+      body: 'Unsupported Request Method'
+    };
+  }
+
+  try {
+    const { attendees: members, id, time, topic } = JSON.parse(event.body);
+    const attendees = await getMobAttendees(members);
+    const title = topic.replace(/\n|\r/g, '');
+
+    const res = await fetch('https://slack.com/api/chat.scheduleMessage', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        channel: process.env.CHANNEL,
+        text: `This is a reminder the *_${title}_* mob will start in 10 minutes ${attendees}`,
+        post_at: new Date(time).getTime() / 1000.0
+      })
+    });
+
+    callback(null, { statusCode: 204, body: 'Success' });
+  } catch (e) {
+    callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e });
+  }
+};
--- functions/mob.js
+++ functions/mob.js
@@ -3,8 +3,32 @@ const fetch = require('node-fetch').default;
 
 const getMobs = async () => {
   const mobUrl = 'https://social.vehikl.com/social_mob/day';
-  const mob = await fetch(mobUrl).then((res) => res.json());
-  return mob;
+  const mobs = await fetch(mobUrl).then((res) => res.json());
+  return mobs;
+};
+
+const convertTime12to24 = (day, time12h) => {
+  const [time, modifier] = time12h.split(' ');
+
+  let [hours, minutes] = time.split(':');
+
+  if (hours === '12') {
+    hours = '00';
+  }
+
+  if (modifier === 'pm') {
+    hours = parseInt(hours, 10) + 12;
+  }
+
+  if (minutes === '00') {
+    hours = parseInt(hours) - 1;
+    minutes = parseInt(minutes) + 50;
+  } else {
+    minutes = parseInt(minutes) - 10;
+  }
+
+  const reminder = new Date(`${day}T${hours}:${minutes}`);
+  return reminder;
 };
 
 exports.handler = async function (event, context, callback) {
@@ -20,6 +44,24 @@ exports.handler = async function (event, context, callback) {
   try {
     const mobs = await getMobs();
     let header = ':boom: *Social Mobs* happening today: :boom:';
+
+    if (mobs.length > 0) {
+      mobs.forEach(({ attendees, date, id, owner, start_time, topic }) => {
+        fetch(
+          `https://${process.env.NAME}.netlify.app/.netlify/functions/mob-scheduler`,
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id,
+              topic,
+              time: convertTime12to24(date, start_time),
+              attendees: [owner.name, ...attendees.map(({ name }) => name)]
+            })
+          }
+        );
+      });
+    }
 
     const res = await fetch('https://slack.com/api/chat.postMessage', {
       method: 'POST',
