b0b3aa170d9e824ecff9d7c7349657fb54daa9da
Tyler Bauman <t.bauman@vehikl.com>
2021-06-08 09:03:36 -0400

    update blocks ui for mobs/mob-added

--- functions/mob-added.js
+++ functions/mob-added.js
@@ -1,6 +1,8 @@
 // @ts-check
 const fetch = require('node-fetch').default;
 const { CHANNEL, TOKEN } = process.env;
+const helpers = require('../helpers.js');
+
 
 exports.handler = async function (event, context, callback) {
   if (event.httpMethod !== 'POST') {
@@ -12,7 +14,23 @@ exports.handler = async function (event, context, callback) {
   const session = JSON.parse(event.body.trim());
   callback(null, { statusCode: 204, body: 'Success' });
 
+  const attendees = session.attendee_limit != null `${session.attendee_limit} attendees available`
+
   try {
+    fetch(
+      `https://${process.env.NAME}.netlify.app/.netlify/functions/session-scheduler`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          session: session.id,
+          title: session.title,
+          time: helpers.convertTime12to24(session.date, session.start_time),
+          attendees: [session.owner.name, ...session.attendees.map(({ name }) => name)]
+        })
+      }
+    )
+
     const res = await fetch('https://slack.com/api/chat.postMessage', {
       method: 'POST',
       headers: {
@@ -23,25 +41,55 @@ exports.handler = async function (event, context, callback) {
         channel: CHANNEL,
         blocks: [
           {
-            type: 'section',
+            type: "header",
             text: {
-              type: 'mrkdwn',
-              text: ':boom: A `NEW` session has been added for today :boom:'
+              type: "plain_text",
+              text: session.title,
+              emoji: true
             }
           },
-          { type: 'divider' },
           {
-            type: 'section',
+            type: "section",
             block_id: `${session.id}`,
             text: {
-              type: 'mrkdwn',
-              text: `:bulb: <https://growth.vehikl.com/growth_sessions/${session.id}| ${session.title}> \n :watch: ${session.start_time} - ${session.end_time} \n :busts_in_silhouette: ${session.attendee_limit || 'nth of'} attendees available \n :round_pushpin: ${session.location}`
+              type: "mrkdwn",
+              text: `Hosted by: \n ${session.owner.name} \n ${session.start_time} - ${session.end_time} \n ${attendees}`
             },
-            accessory: {
-              type: 'image',
-              image_url: session.owner.avatar,
-              alt_text: session.owner.name
+            "accessory": {
+              type: "image",
+              "image_url": session.owner.avatar,
+              "alt_text": session.owner.name
             }
+          },
+          {
+            type: "context",
+            "elements": [
+              {
+                type: "plain_text",
+                text: session.topic,
+                emoji: true
+              }
+            ]
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `:round_pushpin: ${session.location}`
+            },
+            "accessory": {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Join Growth Session",
+              },
+              "value": "click_me_123",
+              "url": `https://growth.vehikl.com/growth_sessions/${session.id}`,
+              "action_id": "button-action"
+            }
+          },
+          {
+            type: "divider"
           }
         ]
       })
