319289a48496226cc99bf885d1818072fe3f1c38
Tyler Bauman <t.bauman@vehikl.com>
2023-12-04 15:41:33 -0500

    enable mob added

--- functions/mob-added.js
+++ functions/mob-added.js
@@ -1,30 +1,37 @@
 // @ts-check
-const fetch = require('node-fetch').default;
+const fetch = require("node-fetch").default;
 const { CHANNEL, TOKEN, NAME } = process.env;
-const helpers = require('../helpers.js');
-
+const helpers = require("../helpers.js");
 
 exports.handler = async function (event, context, callback) {
-  if (event.httpMethod !== 'POST') return { statusCode: 503, body: 'Unsupported Request Method' };
-  callback(null, { statusCode: 204, body: 'Success' });
+  if (event.httpMethod !== "POST")
+    return { statusCode: 503, body: "Unsupported Request Method" };
+  callback(null, { statusCode: 204, body: "Success" });
   const session = JSON.parse(event.body.trim());
-  const attendees = session.attendee_limit != null 
-    ? `${session.attendee_limit} attendees available` 
-    : 'Unlimited attendees available'
+  const attendees =
+    session.attendee_limit != null
+      ? `${session.attendee_limit} attendees available`
+      : "Unlimited attendees available";
 
   try {
-    // fetch(`https://${NAME}.netlify.app/.netlify/functions/mob-scheduler`, {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({
-    //     id: session.id,
-    //     title: session.title,
-    //     time: helpers.convertTime12to24(session.date, session.start_time),
-    //     attendees: [session.owner.name, ...session.attendees.map(({ name }) => name)]
-    //   })
-    // })
-    
-    const headers = { Authorization: `Bearer ${TOKEN}`, 'Content-Type': 'application/json' };
+    fetch(`https://${NAME}.netlify.app/.netlify/functions/mob-scheduler`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: session.id,
+        title: session.title,
+        time: helpers.convertTime12to24(session.date, session.start_time),
+        attendees: [
+          session.owner.name,
+          ...session.attendees.map(({ name }) => name)
+        ]
+      })
+    });
+
+    const headers = {
+      Authorization: `Bearer ${TOKEN}`,
+      "Content-Type": "application/json"
+    };
     const blocks = [
       {
         type: "header",
@@ -41,15 +48,15 @@ exports.handler = async function (event, context, callback) {
           type: "mrkdwn",
           text: `Hosted by: \n ${session.owner.name} \n ${session.start_time} - ${session.end_time} \n ${attendees}`
         },
-        "accessory": {
+        accessory: {
           type: "image",
-          "image_url": session.owner.avatar,
-          "alt_text": session.owner.name
+          image_url: session.owner.avatar,
+          alt_text: session.owner.name
         }
       },
       {
         type: "context",
-        "elements": [
+        elements: [
           {
             type: "plain_text",
             text: session.topic,
@@ -63,15 +70,15 @@ exports.handler = async function (event, context, callback) {
           type: "plain_text",
           text: `:round_pushpin: ${session.location}`
         },
-        "accessory": {
+        accessory: {
           type: "button",
           text: {
             type: "plain_text",
-            text: "Join Growth Session",
+            text: "Join Growth Session"
           },
-          "value": "click_me_123",
-          "url": `https://growth.vehikl.com/growth_sessions/${session.id}`,
-          "action_id": "button-action"
+          value: "click_me_123",
+          url: `https://growth.vehikl.com/growth_sessions/${session.id}`,
+          action_id: "button-action"
         }
       },
       {
@@ -79,14 +86,14 @@ exports.handler = async function (event, context, callback) {
       }
     ];
 
-    const res = await fetch('https://slack.com/api/chat.postMessage', {
-      method: 'POST',
+    const res = await fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
       headers,
       body: JSON.stringify({ channel: CHANNEL, blocks })
     });
 
     if (!res.ok) throw new Error(res.statusText);
   } catch (e) {
-    callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e });
+    callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
   }
 };
